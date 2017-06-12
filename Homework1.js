ebc2f863babcc060cda5eb1cdee668d69bef0b9f
Sherri O'Connor <oconnor.sherri@gmail.com>
2017-06-12 14:13:20 -0400

    finished question 2

--- Homework1.js
+++ Homework1.js
@@ -5,7 +5,9 @@
     document.getElementById('result').innerHTML = (parseInt(var1) + parseInt (var2));
   }
 
-//Challenge 1: Create a script with 2 variables, each assigned to a number. Add them together and output the result to the console. Now do the same with two strings.
+//Challenge 1: Create a script with 2 variables, each assigned to a number.
+//Add them together and output the result to the console.
+// Now do the same with two strings.
 
 var x=2
 var y=4
@@ -14,3 +16,13 @@ console.log (x + y);
 var name1="Sally"
 var name2="Harry"
 console.log (name1 + " met " + name2);
+
+//Create a multidimensional array related to a subject that interests you.
+//Output two of the items in sub-arrays to the console.
+
+var slr = ["Canon", "Nikon", "Sony"];
+var mediumFormat = ["Hasselblad", "Mamiya", "Pentax"];
+var cameras = [slr, mediumFormat];
+cameras
+cameras [2][0]
+cameras [1][1]
