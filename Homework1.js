d1fe6383c7dccfd0383807e96a5cbb39d10c6b6e
Sherri O'Connor <oconnor.sherri@gmail.com>
2017-06-12 13:54:48 -0400

    finished question 1

--- Homework1.js
+++ Homework1.js
@@ -1,1 +1,12 @@
-Homework1.js
+
+  function addNums(){
+    var var1 = document.getElementById('var1').value;
+    var var2 = document.getElementById('var2').value;
+    document.getElementById('result').innerHTML = (parseInt(var1) + parseInt (var2));
+  }
+
+//Challenge 1: Create a script with 2 variables, each assigned to a number. Add them together and output the result to the console. Now do the same with two strings.
+
+var x=2
+var y=4
+console.log (x + y);
